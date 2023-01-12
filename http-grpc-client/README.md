6d3309d570cfcb616e0858e16bcbf09920f8096b
Nathan Prat <nathan.prat@gmail.com>
2023-01-12 19:00:09 +0100

    ocw-common: refactor the http client [1]
    refactor as a workspace/standalone project

---
+++ http-grpc-client/README.md
@@ -0,0 +1,14 @@
+#
+
+TODO the http client is used both:
+- from the pallets/ in this repo
+- as a dependency of `lib-garble-rs`
+
+Be **REALLY** to make sure this client is compatible with no_std/sgx.
+
+NOTE: it MUST use Integritee's `itc_rest_client` else the pallets tests fail with
+```
+[2023-01-12T16:49:35Z ERROR pallet_ocw_garble::pallet] [ocw-garble] ipfs call ipfs_cat error: ResponseError { err: IO(Os { code: 11, kind: WouldBlock, message: "Resource temporarily unavailable" }) }
+thread 'tests::test_garble_and_strip_display_circuits_package_signed_ok' panicked at 'message_reply failed!: IpfsCallError', interstellar-pallets/pallets/ocw-garble/src/lib.rs:683:14
+```
+(even if the node/worker seem to be working fine)
--- http-grpc-client/src/lib.rs
+++ http-grpc-client/src/lib.rs
@@ -1,26 +1,22 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use bytes::{Buf, BufMut};
+extern crate alloc;
 
-// #[cfg(all(not(feature = "std"), feature = "sgx"))]
-// use http::{
-// 	header::{HeaderName, AUTHORIZATION, CONTENT_LENGTH, CONTENT_TYPE, USER_AGENT},
-// 	HeaderValue,
-// };
-// #[cfg(all(not(feature = "std"), feature = "sgx"))]
-// use http_req::{
-// 	request::{Method, Request},
-// 	response::{Headers, Response},
-// 	uri::Uri,
-// };
+// https://github.com/integritee-network/worker/blob/master/core/rest-client/src/lib.rs#L30
 #[cfg(all(not(feature = "std"), feature = "sgx"))]
-use itc_rest_client::http_client::HttpClient;
+use http_req_sgx as http_req;
 #[cfg(all(not(feature = "std"), feature = "sgx"))]
-use itc_rest_client::http_client::SendHttpRequest;
-#[cfg(all(not(feature = "std"), feature = "sgx"))]
-use itc_rest_client::sgx_reexport_prelude::*;
+use url_sgx as url;
+
 #[cfg(not(feature = "sgx"))]
-use sp_runtime::offchain::{http, Duration};
+use http_req_std as http_req;
+
+use alloc::string::ToString;
+use alloc::vec::Vec;
+use bytes::{Buf, BufMut};
+use core::time::Duration;
+use itc_rest_client::http_client::HttpClient;
+use itc_rest_client::http_client::SendHttpRequest;
 
 // we CAN NOT just send the raw encoded protobuf(eg using GarbleIpfsRequest{}.encode())
 // b/c that returns errors like
@@ -132,192 +128,28 @@ pub fn decode_rpc_json<T: codec::Decode>(
     T::decode(&mut data_slice).expect("decode failed")
 }
 
-/// This function uses the `offchain::http` API to query the remote endpoint information,
-///   and returns the JSON response as vector of bytes.
-///
-/// return:
-/// - the body, as raw bytes
-/// - the Content-Type Header: needed to know how to deserialize(cf decode_body)
-#[cfg(not(feature = "sgx"))]
 pub fn fetch_from_remote_grpc_web(
     body_bytes: bytes::Bytes,
     url: &str,
     request_content_type: ContentType,
-) -> Result<(bytes::Bytes, ContentType), http::Error> {
-    // We want to keep the offchain worker execution time reasonable, so we set a hard-coded
-    // deadline to 5s to complete the external call.
-    // You can also wait idefinitely for the response, however you may still get a timeout
-    // coming from the host machine.
-    let deadline = sp_io::offchain::timestamp().add(Duration::from_millis(10_000));
-
-    log::info!(
-        "fetch_from_remote_grpc_web: url = {}, sending body b64 = {}",
-        url,
-        base64::encode(&body_bytes)
-    );
-
-    // Initiate an external HTTP GET request.
-    // This is using high-level wrappers from `sp_runtime`, for the low-level calls that
-    // you can find in `sp_io`. The API is trying to be similar to `reqwest`, but
-    // since we are running in a custom WASM execution environment we can't simply
-    // import the library here.
-    //
-    // cf https://github.com/hyperium/tonic/blob/master/tonic-web/tests/integration/tests/grpc_web.rs
-    // syntax = "proto3";
-    // package test;
-    // service Test {
-    //		rpc SomeRpc(Input) returns (Output);
-    // -> curl http://127.0.0.1:3000/test.Test/SomeRpc
-    //
-    // NOTE application/grpc-web == application/grpc-web+proto
-    //      application/grpc-web-text = base64
-    //
-    // eg:
-    // printf '\x00\x00\x00\x00\x05\x08\xe0\x01\x10\x60' | curl -skv -H "Content-Type: application/grpc-web+proto" -H "X-Grpc-Web: 1" -H "Accept: application/grpc-web-text+proto" -X POST --data-binary @- http://127.0.0.1:3000/interstellarpbapigarble.SkcdApi/GenerateSkcdDisplay
-    let mut request = http::Request::post(url, sp_std::vec![body_bytes]);
-
-    match request_content_type {
-        ContentType::GrpcWeb => {
-            request = request.add_header("Content-Type", "application/grpc-web");
-            request = request.add_header("X-Grpc-Web", "1");
-        }
-        ContentType::Json => {
-            request = request.add_header("Content-Type", "application/json;charset=utf-8");
-        }
-        _ => panic!("request_content_type SHOULD be Json or GrpcWeb"),
-    }
-
-    // We set the deadline for sending of the request, note that awaiting response can
-    // have a separate deadline. Next we send the request, before that it's also possible
-    // to alter request headers or stream body content in case of non-GET requests.
-    // NOTE: 'http_request_start can be called only in the offchain worker context'
-    let pending = request
-        .deadline(deadline)
-        .send()
-        .map_err(|_| http::Error::IoError)?;
-
-    // The request is already being processed by the host, we are free to do anything
-    // else in the worker (we can send multiple concurrent requests too).
-    // At some point however we probably want to check the response though,
-    // so we can block current thread and wait for it to finish.
-    // Note that since the request is being driven by the host, we don't have to wait
-    // for the request to have it complete, we will just not read the response.
-    let mut response = pending
-        .try_wait(deadline)
-        .map_err(|_| http::Error::DeadlineReached)??;
-
-    let response_code = response.code;
-    let content_type = response.headers().find("content-type").unwrap();
-    log::info!(
-        "[fetch_from_remote_grpc_web] status code: {}, content_type: {}",
-        response_code,
-        content_type
-    );
-    let grpc_content_type = match content_type {
-        // yes, "application/grpc-web" and "application/grpc-web+proto" use the same encoding
-        "application/grpc-web" => ContentType::GrpcWeb,
-        "application/grpc-web+proto" => ContentType::GrpcWeb,
-        // BUT "application/grpc-web-text+proto" is base64 encoded
-        "application/grpc-web-text+proto" => ContentType::GrpcWebTextProto,
-        // classic JSON
-        "application/json" => ContentType::Json,
-        "application/json; charset=utf-8" => ContentType::Json,
-        _ => ContentType::Unknown,
-    };
-    // DEBUG: list headers
-    let mut headers_it = response.headers().into_iter();
-    while headers_it.next() {
-        let header = headers_it.current().unwrap();
-        log::info!(
-            "[fetch_from_remote_grpc_web] header: {} {}",
-            header.0,
-            header.1
-        );
-    }
-
-    // Let's check the status code before we proceed to reading the response.
-    if response.code != 200 {
-        log::warn!(
-            "[fetch_from_remote_grpc_web] Unexpected status code: {}",
-            response.code
-        );
-        return Err(http::Error::Unknown);
-    }
-
-    let response_bytes = response.body().collect::<bytes::Bytes>();
-    Ok((response_bytes, grpc_content_type))
-}
-
-#[cfg(all(not(feature = "std"), feature = "sgx"))]
-pub fn fetch_from_remote_grpc_web(
-    body_bytes: bytes::Bytes,
-    url: &str,
-    request_content_type: ContentType,
+    deadline_duration: Duration,
 ) -> Result<(bytes::Bytes, ContentType), itc_rest_client::error::Error> {
-    use sgx_tstd::{collections::HashMap, string::ToString};
-
-    // We want to keep the offchain worker execution time reasonable, so we set a hard-coded
-    // deadline to 5s to complete the external call.
-    // You can also wait idefinitely for the response, however you may still get a timeout
-    // coming from the host machine.
-    let deadline = 10u64; // in seconds(s)
-
     log::info!(
         "fetch_from_remote_grpc_web: url = {}, sending body b64 = {}",
         url,
         base64::encode(&body_bytes)
     );
-
-    // fn add_to_headers(
-    // 	headers: &mut http_req::response::Headers,
-    // 	key: http::header::HeaderName,
-    // 	value: http::HeaderValue,
-    // ) {
-    // 	let header_value_str = value.to_str();
-
-    // 	match header_value_str {
-    // 		Ok(v) => {
-    // 			headers.insert(key.as_str(), v);
-    // 		},
-    // 		Err(e) => {
-    // 			log::error!("Failed to add header to request: {:?}", e);
-    // 		},
-    // 	}
-    // }
-
-    // fn my_headers() -> http_req::response::Headers {
-    // 	let mut headers = http_req::response::Headers::new();
-    // 	// add_to_headers(
-    // 	// 	&mut headers,
-    // 	// 	"Content-Type",
-    // 	// 	http::HeaderValue::from_str().unwrap(),
-    // 	// );
-    // 	// add_to_headers(&mut headers, "X-Grpc-Web", http::HeaderValue::from_str("1").unwrap());
-    // 	headers.insert("Content-Type", "application/grpc-web");
-    // 	headers.insert("X-Grpc-Web", "1");
-    // 	headers
-    // }
 
     let http_client = HttpClient::new(
         itc_rest_client::http_client::DefaultSend {},
         false,
-        Some(sgx_tstd::time::Duration::from_secs(deadline)),
+        Some(deadline_duration),
         None,
         None,
     );
 
     let url = url::Url::parse(url).unwrap();
-    // let (response, encoded_body) = http_client
-    // 	.send_request::<(), sgx_tstd::vec::Vec<u8>>(
-    // 		url,
-    // 		http_req::request::Method::POST,
-    // 		(),
-    // 		None,
-    // 		Some(sgx_tstd::vec![body_bytes]),
-    // 	)
-    // 	.unwrap();
 
-    // TODO .map_err(http::Error::HttpReqError)?
     let uri = http_req::uri::Uri::try_from(url.as_str()).unwrap();
 
     let mut request = http_req::request::Request::new(&uri);
@@ -338,9 +170,7 @@ pub fn fetch_from_remote_grpc_web(
     request.body(&body_bytes);
     request_headers.insert("Content-Length", &body_bytes.len().to_string());
 
-    request.headers(HashMap::from(request_headers));
-
-    let mut response_bytes = sgx_tstd::vec::Vec::new();
+    let mut response_bytes = Vec::new();
     // let response = http_client.execute_send_request(&mut request, &mut write)?;
     let response = request.send(&mut response_bytes).unwrap(); // TODO .map_err(Error::HttpReqError)
 
@@ -361,12 +191,6 @@ pub fn fetch_from_remote_grpc_web(
         "application/json; charset=utf-8" => ContentType::Json,
         _ => ContentType::Unknown,
     };
-    // TODO DEBUG: list headers
-    // let mut headers_it = response.headers().iter();
-    // while headers_it.next() {
-    // 	let header = headers_it.current().unwrap();
-    // 	log::info!("[fetch_from_remote_grpc_web] header: {} {}", header.0, header.1);
-    // }
 
     // Let's check the status code before we proceed to reading the response.
     if !response.status_code().is_success() {
@@ -380,7 +204,6 @@ pub fn fetch_from_remote_grpc_web(
         ));
     }
 
-    // let response_bytes = response.body().collect::<bytes::Bytes>();
     return Ok((bytes::Bytes::from(response_bytes), content_type));
 }
 
